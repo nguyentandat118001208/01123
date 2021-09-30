317a521f6665d46cb127d7749c1b6b79a648ce02
datnguyenuhth <datnguyenuhth@gmail.com>
2021-09-30 16:32:53 +0700

    cai nay can hoan thanh nhe Giang

--- Models/Properties/AssemblyInfo.cs
+++ Models/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("43406496-5893-47ad-8a59-8261d226680d")]
+[assembly: Guid("df4a6581-edf3-46bf-a6d9-76466f6e158d")]
 
 // Version information for an assembly consists of the following four values:
 //
